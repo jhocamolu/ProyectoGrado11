2e7167f1b228ff6210531fc2462d965e73956fdd
Jhonatan Camilo Moreno Luna <jhocamolu2010@gmail.com>
2026-04-12 20:47:46 -0500

    Ajustes en interfaces

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -10,6 +10,7 @@ $routes->get('/', 'Home::index');
 // Solo usuarios logueados pueden entrar a cualquier ruta de este grupo
 $routes->group('admin', ['filter' => 'session'], function ($routes) {
     $routes->get('/', 'Home::Admin');
+    $routes->resource('users', ['controller' => 'UsersController']);
     $routes->get('estudiantes', 'EstudiantesController::index');
 });
 service('auth')->routes($routes);
